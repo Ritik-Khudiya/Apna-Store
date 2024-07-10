f6d29efe4cd4ed44eac16e775bcf107d366b04c7
Ritik-Khudiya <ritikkumar5960@gmail.com>
2024-07-10 13:18:55 +0530

    updated

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const jsonServer=require('json-server');
 
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.resolve('frontend/build')));
 
 const apiServer = jsonServer.create();
 const apiRouter = jsonServer.router('db.json');
@@ -23,10 +23,15 @@ apiServer.listen(5000, () => {
 });
 
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+// });
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname ,'frontend/build/index.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
